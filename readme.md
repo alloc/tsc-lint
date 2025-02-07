0bd188a71d17d0932747feb3e72c99cc76dc804c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-07 14:58:43 -0500

    init

---
+++ readme.md
@@ -0,0 +1,36 @@
+# tsc-lint
+
+Run `tsc` on multiple tsconfigs in parallel. Your `tsconfig.json` files are discovered automatically, and `.gitignore` files are respected. The `--emitDeclarationOnly` option is added for you, and `node_modules/.tsc-lint` is used as the output directory.
+
+**Note:** [Project references][1] are not supported.
+
+```sh
+pnpm add tsc-lint -D
+```
+
+Update your project's `package.json` to add a `lint` script.
+
+```json
+{
+  "scripts": {
+    "lint": "tsc-lint"
+  }
+}
+```
+
+By default, the working directory is searched for `tsconfig.json` files. You can pass one or more different directories as an argument, and those will be searched instead.
+
+```sh
+# Lint only your ./test/ folder.
+pnpm lint test
+```
+
+Importantly, you **must not** have `noEmit` in your `tsconfig.json` files. The emit step is critical for catching certain errors, which is why `tsc-lint` uses the `--emitDeclarationOnly` option instead.
+
+## Options
+
+```sh
+tsc-lint --help
+```
+
+[1]: https://www.typescriptlang.org/docs/handbook/project-references.html
---
+++ src/main.ts
@@ -0,0 +1,234 @@
+#!/usr/bin/env node
+import createDebug from 'debug'
+import ignore from 'ignore'
+import { spawn } from 'node:child_process'
+import { readdirSync, readFileSync, statSync } from 'node:fs'
+import { cpus } from 'node:os'
+import path from 'node:path'
+import util from 'node:util'
+import { parallel, select } from 'radashi'
+import { GlobOptions, globSync } from 'tinyglobby'
+
+const cwd = process.cwd()
+const debug = createDebug('tsc-lint')
+const gitIgnore = ignore()
+
+let tscPath: string | undefined
+
+findUp(cwd, (dir, files) => {
+  debug(`Looking for gitignore${tscPath ? '' : ' or tsc binary'} in ${dir}`)
+
+  if (!tscPath && files.includes('node_modules')) {
+    const potentialTscPath = path.join(dir, 'node_modules/.bin/tsc')
+    if (fileExists(potentialTscPath)) {
+      tscPath = potentialTscPath
+      debug(`Found tsc binary at ${tscPath}`)
+    }
+  }
+
+  if (files.includes('.gitignore')) {
+    const gitIgnorePath = path.join(dir, '.gitignore')
+
+    let patterns = readFileSync(gitIgnorePath, 'utf8').split('\n')
+    if (dir !== cwd) {
+      // For gitignore files from ancestors, ignore any patterns with a leading "/" since those will
+      // usually only apply within the ancestor directory.
+      patterns = patterns.map(pattern => {
+        if (pattern[0] === '#' || /^\s*$/.test(pattern)) {
+          return pattern
+        }
+        let prefix = ''
+        if (pattern[0] === '!') {
+          prefix = '!'
+          pattern = pattern.slice(1)
+        }
+        if (pattern[0] === '/') {
+          return ''
+        }
+        return prefix + pattern
+      })
+    }
+
+    if (debug.enabled) {
+      debug(`Ignoring paths from ${gitIgnorePath}`)
+      debug(
+        select(patterns, pattern =>
+          pattern && pattern[0] !== '#' ? `  ${pattern}` : undefined
+        ).join('\n')
+      )
+    }
+
+    gitIgnore.add(patterns)
+  }
+})
+
+if (!tscPath) {
+  console.error('tsc-lint: tsc binary not found')
+  process.exit(1)
+}
+
+const { positionals, values } = util.parseArgs({
+  allowPositionals: true,
+  options: {
+    ignore: {
+      type: 'string',
+      multiple: true,
+      short: 'i',
+    },
+  },
+})
+
+const globOptions: GlobOptions = (() => {
+  const ignoredPaths = new Set(values.ignore)
+  ignoredPaths.add('**/node_modules/**')
+  return {
+    ignore: [...ignoredPaths],
+    dot: true,
+  }
+})()
+
+for (const gitIgnorePath of globSync('**/.gitignore', globOptions)) {
+  // Looking for nested gitignores.
+  if (gitIgnorePath !== '.gitignore') {
+    const dir = path.dirname(gitIgnorePath)
+    const patterns = readFileSync(gitIgnorePath, 'utf8')
+      .split('\n')
+      .map(line => {
+        if (line[0] === '#' || /^\s*$/.test(line)) {
+          return line
+        }
+        let prefix = ''
+        if (line[0] === '!') {
+          prefix = '!'
+          line = line.slice(1)
+        }
+        if (line[0] === '/') {
+          line = '/' + path.join(dir, line.slice(1))
+        } else {
+          line = path.join(dir, '**', line)
+        }
+        return prefix + line
+      })
+
+    if (debug.enabled) {
+      debug(`Ignoring paths from ${gitIgnorePath}`)
+      debug(
+        select(patterns, pattern =>
+          pattern && pattern[0] !== '#' ? `  ${pattern}` : undefined
+        ).join('\n')
+      )
+    }
+
+    gitIgnore.add(patterns)
+  }
+}
+
+const rootDirs = positionals.length > 0 ? positionals : ['.']
+
+const tsconfigs = rootDirs
+  .flatMap(cwd => {
+    return globSync('**/tsconfig.json', {
+      ...globOptions,
+      absolute: true,
+      cwd,
+    })
+  })
+  .filter(tsconfig => {
+    if (gitIgnore.ignores(path.relative(cwd, tsconfig))) {
+      debug(`Skipped by .gitignore file: ${tsconfig}`)
+      return false
+    }
+    return true
+  })
+
+const nodeModulesDir = path.dirname(tscPath)
+const tscOutputDir = path.join(nodeModulesDir, '.tsc-lint')
+
+parallel({ limit: cpus().length }, tsconfigs, tsconfig => {
+  return new Promise<void>((resolve, reject) => {
+    const tsconfigDir = path.dirname(tsconfig)
+    console.log(`Using ${path.relative(cwd, tsconfig)}`)
+
+    // Handle cases where a package depends on a different TypeScript version.
+    let ownTscPath: string | undefined
+    findUp(tsconfigDir, (dir, files) => {
+      if (files.includes('node_modules')) {
+        const tscPath = path.join(dir, 'node_modules/.bin/tsc')
+        if (fileExists(tscPath)) {
+          ownTscPath = tscPath
+          return true
+        }
+      }
+    })
+
+    const child = spawn(
+      ownTscPath ?? tscPath!,
+      [
+        '--project',
+        tsconfig,
+        '--outDir',
+        tscOutputDir,
+        '--declaration',
+        '--emitDeclarationOnly',
+      ],
+      {
+        stdio: ['ignore', 'inherit', 'pipe'],
+      }
+    )
+
+    child.stderr!.setEncoding('utf8')
+    child.stderr!.on('data', (data: string) => {
+      // Resolve relative paths to ones relative to the current working directory.
+      data = data.replace(
+        /^([^\S]+)(:\d+:\d+)\b/gm,
+        (match, file, location) => {
+          file = path.resolve(tsconfigDir, file)
+          if (fileExists(file)) {
+            return `${path.relative(cwd, file)}${location}`
+          }
+          return match
+        }
+      )
+      process.stderr.write(data)
+    })
+
+    child.on('error', reject)
+    child.on('close', code => {
+      if (code !== 0) {
+        process.exitCode = 1
+      }
+      resolve()
+    })
+  })
+}).catch(error => {
+  const { errors } = error as AggregateError
+  for (const error of errors) {
+    console.error(error)
+  }
+  process.exit(1)
+})
+
+function fileExists(path: string) {
+  try {
+    const stats = statSync(path)
+    return stats.isFile() || stats.isSymbolicLink()
+  } catch (error) {
+    return false
+  }
+}
+
+function findUp(
+  dir: string,
+  callback: (dir: string, files: string[]) => boolean | void
+) {
+  const { root } = path.parse(dir)
+  while (true) {
+    if (callback(dir, readdirSync(dir))) {
+      return
+    }
+    if (dir === root) {
+      return
+    }
+    dir = path.dirname(dir)
+  }
+}
